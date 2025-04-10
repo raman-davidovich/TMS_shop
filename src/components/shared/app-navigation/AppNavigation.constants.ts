03f399326f4a918792f55e7f03aae0b3047e842a
Raman Davidovich <raman.davidovich@gmail.com>
2025-04-10 06:46:21 +0300

    Change structure types and constants

--- src/components/shared/app-navigation/AppNavigation.constants.ts
+++ src/components/shared/app-navigation/AppNavigation.constants.ts
@@ -1,4 +1,6 @@
-export const MENU_ITEMS = {
+import { AppNavigationItemType } from './AppNavigation.types'
+
+export const APP_NAVIGATION_ITEMS = {
   HOME: 'Home',
   SHOP: 'Shop',
   ACCOUNT: 'Account',
@@ -7,7 +9,16 @@ export const MENU_ITEMS = {
   DOCS: 'Docs/Components'
 } as const
 
+export const MENU_ITEMS: AppNavigationItemType[] = [
+  APP_NAVIGATION_ITEMS.HOME,
+  APP_NAVIGATION_ITEMS.SHOP,
+  APP_NAVIGATION_ITEMS.ACCOUNT,
+  APP_NAVIGATION_ITEMS.PAGES,
+  APP_NAVIGATION_ITEMS.BLOG,
+  APP_NAVIGATION_ITEMS.DOCS
+] as const
+
 export const APP_NAVIGATION_COLOR_TYPES = {
-  PRIMARY: 'primaty',
+  PRIMARY: 'primary',
   ADDITIONAL: 'additional'
 } as const
--- src/components/shared/app-navigation/AppNavigation.types.ts
+++ src/components/shared/app-navigation/AppNavigation.types.ts
@@ -1,3 +1,4 @@
-import { APP_NAVIGATION_COLOR_TYPES } from './AppNavigation.constants'
+import { APP_NAVIGATION_COLOR_TYPES, APP_NAVIGATION_ITEMS } from './AppNavigation.constants'
 
+export type AppNavigationItemType = (typeof APP_NAVIGATION_ITEMS)[keyof typeof APP_NAVIGATION_ITEMS]
 export type AppNavigationColorType = (typeof APP_NAVIGATION_COLOR_TYPES)[keyof typeof APP_NAVIGATION_COLOR_TYPES]
