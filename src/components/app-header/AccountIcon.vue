aebbf13027ef9745fa36401d7657c431deb5d37c
Raman Davidovich <raman.davidovich@gmail.com>
2025-04-16 05:54:50 +0300

    Implement icon components

---
+++ src/components/app-header/AccountIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="icon-container">
+      <svg width="32" height="32" viewBox="0 0 26 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M19 9C19 14 16 18 13 18C10 18 7 14 7 9C7 4 9 1 13 1C17 1 19 4 19 9ZM1 28H25C25 19 19 18 13 18C7 18 1 19 1 28Z"
+          stroke="#737373"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .icon-container {
+    display: flex;
+
+    svg {
+      flex-shrink: 0;
+      height: 100%;
+      width: 100%;
+    }
+  }
+</style>
---
+++ src/components/app-header/BurgerMenuIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="icon-container">
+      <svg width="32" height="32" viewBox="0 0 28 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M0.666992 0.934814H27.3337V3.60148H0.666992V0.934814ZM7.33366 7.60148H27.3337V10.2681H7.33366V7.60148ZM15.667 14.2681H27.3337V16.9348H15.667V14.2681Z"
+          fill="#737373"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .icon-container {
+    display: flex;
+
+    svg {
+      flex-shrink: 0;
+      height: 100%;
+      width: 100%;
+    }
+  }
+</style>
