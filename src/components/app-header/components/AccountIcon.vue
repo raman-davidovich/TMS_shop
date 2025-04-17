dd648d356b13bb4b82c09d13764a2b511899717a
Raman Davidovich <raman.davidovich@gmail.com>
2025-04-17 11:24:41 +0300

    Change icons template and left only svg element

--- src/components/app-header/components/AccountIcon.vue
+++ src/components/app-header/components/AccountIcon.vue
@@ -1,27 +1,11 @@
 <template>
-  <div>
-    <div class="icon-container">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 29" fill="none">
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 9c0 5-3 9-6 9s-6-4-6-9 2-8 6-8 6 3 6 8ZM1 28h24c0-9-6-10-12-10S1 19 1 28Z"
-        />
-      </svg>
-    </div>
-  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 29" fill="none">
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M19 9c0 5-3 9-6 9s-6-4-6-9 2-8 6-8 6 3 6 8ZM1 28h24c0-9-6-10-12-10S1 19 1 28Z"
+    />
+  </svg>
 </template>
-
-<style scoped lang="scss">
-  .icon-container {
-    display: flex;
-
-    svg {
-      flex-shrink: 0;
-      height: 100%;
-      width: 100%;
-    }
-  }
-</style>
