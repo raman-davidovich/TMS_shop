346057e2725937aeb7fc253798320271d9c2e9be
Raman Davidovich <raman.davidovich@gmail.com>
2025-04-23 05:19:51 +0300

    Create router

---
+++ src/router/index.ts
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { ROUTE_NAMES } from './router.constants'
+import AccountPage from '../pages/AccountPage.vue'
+import CartPage from '../pages/CartPage.vue'
+import FavoritePage from '../pages/FavoritePage.vue'
+import HomePage from '../pages/HomePage.vue'
+import ShopPage from '../pages/ShopPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: ROUTE_NAMES.HOME,
+    component: HomePage
+  },
+  {
+    path: '/shop',
+    name: ROUTE_NAMES.SHOP,
+    component: ShopPage
+  },
+  {
+    path: '/account',
+    name: ROUTE_NAMES.ACCOUNT,
+    component: AccountPage
+  },
+  {
+    path: '/favorite',
+    name: ROUTE_NAMES.FAVORITE,
+    component: FavoritePage
+  },
+  {
+    path: '/cart',
+    name: ROUTE_NAMES.CART,
+    component: CartPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
