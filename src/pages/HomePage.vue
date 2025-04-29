517294364a8bafd0d0834f084b46038a3420f642
Raman Davidovich <raman.davidovich@gmail.com>
2025-04-29 09:51:45 +0300

    Add additional sections to HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
   import AppHero from '@/components/app-hero/AppHero.vue'
   import { IMAGES } from '@/components/app-hero/AppHero.constants'
+  import SectionWrapper from '@/components/shared/section-wrapper/SectionWrapper.vue'
 </script>
 
 <template>
-  <AppHero title="New Collection" :imageURL="IMAGES.HOME" withButton />
+  <main>
+    <AppHero title="New Collection" :imageURL="IMAGES.HOME" withButton />
+    <SectionWrapper />
+    <SectionWrapper title="Featured Products" isMarginNarrow />
+    <SectionWrapper title="Latest Products" />
+  </main>
 </template>
