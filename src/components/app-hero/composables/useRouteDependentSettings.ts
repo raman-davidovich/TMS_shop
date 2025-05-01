468d85ccfe5970569cc91b6f7a20e5f5fe885209
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-01 12:50:45 +0300

    Create isShopRoute composable

--- src/components/app-hero/composables/useRouteDependentSettings.ts
+++ src/components/app-hero/composables/useRouteDependentSettings.ts
@@ -5,28 +5,29 @@ import { ROUTE_NAMES } from '../../../router/router.constants'
 export const useRouteDependentSettings = () => {
   const route: RouteLocationNormalizedLoaded = useRoute()
   const isTablet = ref<boolean>(false)
+  const isShopRoute = computed<boolean>(() => route.name === ROUTE_NAMES.SHOP)
 
   const updateMediaQuery = (): void => {
     isTablet.value = window.matchMedia('(max-width: 1024px)').matches
   }
 
-  onMounted(() => {
+  onMounted((): void => {
     updateMediaQuery()
     window.addEventListener('resize', updateMediaQuery)
   })
 
-  onUnmounted(() => {
+  onUnmounted((): void => {
     window.removeEventListener('resize', updateMediaQuery)
   })
 
   const padding = computed<string>(() => {
-    if (route.name === ROUTE_NAMES.SHOP) {
+    if (isShopRoute.value) {
       return isTablet.value ? '75px 0' : '183px 0 75px'
     }
     return '262px 0 134px'
   })
 
-  const marginTop = computed<string>(() => (route.name === ROUTE_NAMES.SHOP ? '0' : '-108px'))
+  const marginTop = computed<string>(() => (isShopRoute.value ? '0' : '-108px'))
 
   return { padding, marginTop }
 }
