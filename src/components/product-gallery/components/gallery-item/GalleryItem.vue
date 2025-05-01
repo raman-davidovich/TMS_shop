806a795b150cc15573b104d732065d477c04de87
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-01 07:31:33 +0300

    Implement GalleryItem component

---
+++ src/components/product-gallery/components/gallery-item/GalleryItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import AppButton from '@/components/shared/app-button/AppButton.vue'
+  import { ROUTE_NAMES } from '@/router/router.constants'
+
+  defineProps<{
+    buttonTitle: string
+    imageURL: string
+  }>()
+</script>
+
+<template>
+  <div class="gallery-item" :style="{ backgroundImage: `url(${imageURL})` }">
+    <AppButton :buttonTitle="buttonTitle" :route="ROUTE_NAMES.SHOP" isTransparent />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .gallery-item {
+    align-items: center;
+    background-size: cover;
+    display: flex;
+    height: 220px;
+    justify-content: center;
+    width: 370px;
+  }
+</style>
