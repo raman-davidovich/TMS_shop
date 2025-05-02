6fd5a832d7ba823151bcfcfc1250124c1a366df7
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-02 06:19:44 +0300

    Add lazy loading for pages components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,35 @@
 import { createRouter, createWebHistory, RouteLocationNormalized, RouteRecordRaw } from 'vue-router'
 import { ROUTE_NAMES } from './router.constants'
-import AccountPage from '../pages/AccountPage.vue'
-import CartPage from '../pages/CartPage.vue'
-import FavoritePage from '../pages/FavoritePage.vue'
-import HomePage from '../pages/HomePage.vue'
-import ShopPage from '../pages/ShopPage.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: ROUTE_NAMES.HOME,
-    component: HomePage,
+    component: () => import('../pages/HomePage.vue'),
     meta: { title: 'Home' }
   },
   {
     path: '/shop',
     name: ROUTE_NAMES.SHOP,
-    component: ShopPage,
+    component: () => import('../pages/ShopPage.vue'),
     meta: { title: 'Shop' }
   },
   {
     path: '/account',
     name: ROUTE_NAMES.ACCOUNT,
-    component: AccountPage,
+    component: () => import('../pages/AccountPage.vue'),
     meta: { title: 'Account' }
   },
   {
     path: '/favorite',
     name: ROUTE_NAMES.FAVORITE,
-    component: FavoritePage,
+    component: () => import('../pages/FavoritePage.vue'),
     meta: { title: 'Favorites' }
   },
   {
     path: '/cart',
     name: ROUTE_NAMES.CART,
-    component: CartPage,
+    component: () => import('../pages/CartPage.vue'),
     meta: { title: 'Cart' }
   }
 ]
