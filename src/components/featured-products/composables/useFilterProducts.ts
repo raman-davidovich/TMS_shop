00255380db7a13467419ec6659c6bd30a36e15d6
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-09 20:01:11 +0300

    Implement FeaturedProducts component

---
+++ src/components/featured-products/composables/useFilterProducts.ts
@@ -0,0 +1,18 @@
+import { FEATURED_PRODUCTS } from '../../shared/app-product-card/AppProductCard.constants'
+import type { FeaturedProductType } from '../../shared/app-product-card/AppProductCard.types'
+import { TABS } from '../components/featured-products-tabs/FeaturedProductsTabs.constants'
+
+export const useFilterProducts = (activeTab: TABS): FeaturedProductType[] => {
+  const products = [...FEATURED_PRODUCTS] as FeaturedProductType[]
+
+  switch (activeTab) {
+    case TABS.FEATURED:
+      return products.filter((product) => product.featured === true)
+    case TABS.NEW:
+      return products.sort((a, b) => b.createdAt.getDate() - a.createdAt.getDate())
+    case TABS.POPULAR:
+      return products.sort((a, b) => b.numberOfSales - a.numberOfSales)
+    default:
+      return products
+  }
+}
