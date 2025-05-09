183300b7714a553e08cf8b3d33077d5a431bd944
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-09 08:11:44 +0300

    Add FeaturedProducts to HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,13 +3,14 @@
   import { APP_HERO_IMAGES } from '@/components/app-hero/AppHero.constants'
   import SectionWrapper from '@/components/shared/section-wrapper/SectionWrapper.vue'
   import ProductGallery from '@/components/product-gallery/ProductGallery.vue'
+  import FeaturedProducts from '@/components/featured-products/FeaturedProducts.vue'
 </script>
 
 <template>
   <main>
     <AppHero title="New Collection" :imageURL="APP_HERO_IMAGES.HOME" withButton />
     <SectionWrapper><ProductGallery /></SectionWrapper>
-    <SectionWrapper title="Featured Products" isMarginNarrow />
+    <SectionWrapper title="Featured Products" isMarginNarrow><FeaturedProducts /></SectionWrapper>
     <SectionWrapper title="Latest Products" />
   </main>
 </template>
