c0ad892abf01cc27b4ca89d93af86e6d22f4c496
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-21 07:14:09 +0300

    Implement SortingBar component

---
+++ src/components/sorting-bar/SortingBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { defineModel } from 'vue'
+  import type { SortOption } from '@/stores/productStore.types'
+  import type { ViewType } from '@/components/product-list/ProductList.types'
+  import SelectGroup from './components/select-group/SelectGroup.vue'
+  import ViewPicker from './components/view-picker/ViewPicker.vue'
+
+  const sortBy = defineModel<SortOption>('sortBy', { required: true })
+  const viewType = defineModel<ViewType>('viewType', { required: true })
+</script>
+
+<template>
+  <div class="sorting-bar">
+    <SelectGroup v-model:sortBy="sortBy" />
+    <ViewPicker v-model:viewType="viewType" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .sorting-bar {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
