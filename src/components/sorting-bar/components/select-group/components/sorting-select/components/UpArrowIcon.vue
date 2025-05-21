b8ac091cf16c983c8a7ae683af63de1b0a1871a0
Raman Davidovich <raman.davidovich@gmail.com>
2025-05-21 07:14:35 +0300

    Implement SelectGroup component

---
+++ src/components/sorting-bar/components/select-group/components/sorting-select/components/UpArrowIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 6">
+    <path
+      fill="currentColor"
+      d="m5.549 0 4.243 4.243-1.415 1.414-2.828-2.829-2.828 2.829-1.415-1.414L5.549 0Z"
+    />
+  </svg>
+</template>
